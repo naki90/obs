fa6aafd5ebf6e593c0f44bb0d7c8101706b0e00e
naki90 <143862130+naki90@users.noreply.github.com>
2023-09-03 17:13:23 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+sudo apt-get update && apt-get upgrade -y
 sudo apt-get install flatpak -y
 sudo apt-get install gnome-software-plugin-flatpak -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
