a30816e21b7df0d8c081c508a2ffe12e7a87e128
naki90 <143862130+naki90@users.noreply.github.com>
2023-09-03 17:14:11 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get update && apt-get upgrade -y
 sudo apt-get install flatpak -y
 sudo apt-get install gnome-software-plugin-flatpak -y
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub com.obsproject.Studio
-flatpak run com.obsproject.Studio
+flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo -y
+flatpak install flathub com.obsproject.Studio -y
+flatpak run com.obsproject.Studio 
